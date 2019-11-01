7976685c65f6e2250f39daf54f1f728b2b61defd
Eyal Kapah <kaycee1@gmail.com>
2019-11-02 00:23:51 +0200

    adding view model

--- MyBlazorDemo/Startup.cs
+++ MyBlazorDemo/Startup.cs
@@ -11,27 +11,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyBlazorDemo.Data;
 using MyBlazorDemo.Data.Interfaces;
+using MyBlazorDemo.ViewModels;
 
 namespace MyBlazorDemo
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-
-            services.AddScoped<ICounterService, CounterService>();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +48,18 @@ namespace MyBlazorDemo
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services.AddScoped<ICounterService, CounterService>();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<CounterViewModel>();
         }
     }
 }
---
+++ MyBlazorDemo/ViewModels/CounterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MyBlazorDemo.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorDemo.ViewModels
+{
+    public class CounterViewModel
+    {
+        public ICounterService CounterService { get; set; }
+
+        public string Title { get; set; } = "Set from code";
+
+        public CounterViewModel(ICounterService counterService)
+        {
+            CounterService = counterService;
+        }
+    }
+}
