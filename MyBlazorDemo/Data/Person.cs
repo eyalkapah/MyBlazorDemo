110fc09609d083844473cd2313d65b31a19b4539
Eyal Kapah <kaycee1@gmail.com>
2019-11-01 23:27:05 +0200

    adding form + validation

---
+++ MyBlazorDemo/Data/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlazorDemo.Data
+{
+    public class Person
+    {
+        [Required]
+        [Range(18, 65)]
+        public int Age { get; set; }
+
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [MaxLength(5)]
+        public string Name { get; set; }
+    }
+}
