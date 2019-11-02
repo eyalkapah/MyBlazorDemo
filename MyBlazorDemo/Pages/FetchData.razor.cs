a8e7b2ad23f452400c1eb2521a66a6b0e820929e
Eyal Kapah <kaycee1@gmail.com>
2019-11-02 19:02:12 +0200

    handle navigation params

--- MyBlazorDemo/Pages/FetchData.razor.cs
+++ MyBlazorDemo/Pages/FetchData.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 using MyBlazorDemo.Data;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,11 @@ namespace MyBlazorDemo.Pages
         [Parameter]
         public string Title { get; set; } = "Default Title";
 
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanges;
+        }
+
         public void Navigate()
         {
             NavigationManager.NavigateTo("/fetchdata/Brian Lagunas/36");
@@ -35,10 +41,19 @@ namespace MyBlazorDemo.Pages
             Forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
 
             Refresh();
+
+            //NavigationManager.LocationChanged += OnLocationChanges;
         }
+
+        private void OnLocationChanges(object sender, LocationChangedEventArgs e) => Refresh();
 
         private void Refresh()
         {
+            var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+            var name = Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(uri.Query).TryGetValue("name", out var title);
+
+            Title = title;
+            //StateHasChanged();
         }
     }
 }
